3f58974074ccef2371bb98a5d416b2f612ce6cba
Raphael Cunha <rbcunhadesign@gmail.com>
2025-03-18 02:06:48 -0400

    Add utility function `cn` for className merging
    Introduce the `cn` function using `clsx` and `tailwind-merge` to simplify and enhance className manipulation. This improves readability and ensures consistent handling of Tailwind classes.

---
+++ resources/js/components/AppSidebar.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import { cn } from '@/lib/utils';
+</script>
+
+<template>
+    <div :class="cn(['fixed inset-y-0 left-0 mt-60 w-64 text-white'])"></div>
+</template>
--- resources/js/layouts/AppLayout.vue
+++ resources/js/layouts/AppLayout.vue
@@ -1,23 +1,18 @@
 <script setup lang="ts">
+import AppSidebar from '@/components/AppSidebar.vue';
 import { Button, Toast } from 'primevue';
-import { ref } from 'vue';
-
-const sidebarOpen = ref(true);
 </script>
 
 <template>
     <div class="relative isolate flex min-h-svh w-full bg-surface-0 max-lg:flex-col lg:bg-surface-100 dark:bg-surface-900 dark:lg:bg-surface-950">
-        <div class="fixed inset-y-0 left-0 w-64 max-lg:hidden"></div>
+        <AppSidebar />
 
         <main class="flex flex-1 flex-col pb-2 lg:min-w-0 lg:pl-64 lg:pr-2 lg:pt-2">
             <div
                 class="lg:shadow-xs grow p-6 lg:rounded-lg lg:bg-surface-0 lg:p-10 lg:ring-1 lg:ring-surface-950/5 dark:lg:bg-surface-900 dark:lg:ring-surface-0/10"
             >
                 <div>
-                    <Button
-                        label="Menu"
-                        @click="sidebarOpen = !sidebarOpen"
-                    />
+                    <Button label="Menu" />
                 </div>
                 <div class="mx-auto max-w-6xl">
                     <slot />
