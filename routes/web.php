7762a2e19c4956729549aeedaa9df4e019c1ab9d
Raphael Cunha <rbcunhadesign@gmail.com>
2025-03-19 02:07:23 -0400

    [WIP] Add dummy route and links for testing

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => 'auth:web'], function () {
     Route::get('/', DashboardController::class)->name('dashboard');
+    Route::get('/pets', fn () => \Inertia\Inertia::render('Pets'))->name('pets');
 });
 
 require __DIR__.'/settings.php';
