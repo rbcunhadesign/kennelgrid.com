c877f731015c6fead1adbcf85ff801134be9e348
Raphael Cunha <rbcunhadesign@gmail.com>
2025-03-19 02:06:29 -0400

    Add reactive sidebar motion with `@vueuse/motion`
    Implemented dynamic sidebar transitions using VueUse Motion to enhance the user experience. Updated `AppSidebar` to bind reactivity to its states.

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -1,7 +1,80 @@
 <script setup lang="ts">
-import { cn } from '@/lib/utils';
+import { useMotion } from '@vueuse/motion';
+import { onMounted, useTemplateRef, watch } from 'vue';
+
+const props = defineProps({
+    open: Boolean,
+    openMobile: Boolean,
+    isMobile: Boolean,
+});
+
+const target = useTemplateRef('sidebar-container');
+
+const motionInstance = useMotion(target, {
+    initial: {
+        x: props.open && !props.isMobile ? 0 : -300,
+    },
+    enter: {
+        x: 0,
+        transition: {
+            type: 'spring',
+            stiffness: 180,
+            damping: 25,
+            mass: 0.5,
+        },
+    },
+    leave: {
+        x: -300,
+        transition: {
+            type: 'spring',
+            stiffness: 180,
+            damping: 25,
+            mass: 0.5,
+        },
+    },
+});
+
+onMounted(() => {
+    if (props.open) {
+        motionInstance.apply('initial');
+    } else {
+        motionInstance.apply('leave');
+    }
+});
+
+watch(
+    () => props.open,
+    (open) => {
+        if (open) {
+            motionInstance.apply('enter');
+        } else {
+            motionInstance.apply('leave');
+        }
+    },
+);
+
+watch(
+    () => props.isMobile,
+    (isMobile) => {
+        if (!isMobile && props.open) {
+            motionInstance.apply('enter');
+        } else {
+            motionInstance.apply('leave');
+        }
+    },
+);
 </script>
 
 <template>
-    <div :class="cn(['fixed inset-y-0 left-0 mt-60 w-64 text-white'])"></div>
+    <div
+        v-if="!props.isMobile"
+        class="fixed inset-y-0 left-0 w-64 text-white"
+        ref="sidebar-container"
+    >
+        <p>open: {{ String(props.open) }}</p>
+        <p>openMobile: {{ String(props.openMobile) }}</p>
+        <p>mobile {{ String(props.isMobile) }}</p>
+    </div>
+
+    <div v-else></div>
 </template>
--- resources/js/layouts/AppLayout.vue
+++ resources/js/layouts/AppLayout.vue
@@ -1,18 +1,85 @@
 <script setup lang="ts">
 import AppSidebar from '@/components/AppSidebar.vue';
+import { useSidebar } from '@/composables/useSidebar';
+import { useMotion } from '@vueuse/motion';
 import { Button, Toast } from 'primevue';
+import { onMounted, unref, useTemplateRef, watch } from 'vue';
+
+const sidebar = useSidebar();
+
+const target = useTemplateRef('main-content');
+
+const motionInstance = useMotion(target, {
+    initial: {
+        paddingLeft: sidebar.open.value && !sidebar.isMobile.value ? 300 : 8,
+    },
+    enter: {
+        paddingLeft: 300,
+        transition: {
+            type: 'spring',
+            stiffness: 180,
+            damping: 25,
+            mass: 0.5,
+        },
+    },
+    leave: {
+        paddingLeft: 8,
+        transition: {
+            type: 'spring',
+            stiffness: 180,
+            damping: 25,
+            mass: 0.5,
+        },
+    },
+});
+
+onMounted(() => {
+    if (sidebar.open.value) {
+        motionInstance.apply('initial');
+    } else {
+        motionInstance.apply('leave');
+    }
+});
+
+watch(sidebar.open, (open) => {
+    if (open) {
+        motionInstance.apply('enter');
+    } else {
+        motionInstance.apply('leave');
+    }
+});
+
+watch(sidebar.isMobile, (isMobile) => {
+    if (isMobile) {
+        motionInstance.apply('leave');
+    }
+
+    if (!isMobile && sidebar.open.value) {
+        motionInstance.apply('enter');
+    }
+});
 </script>
 
 <template>
     <div class="relative isolate flex min-h-svh w-full bg-surface-0 max-lg:flex-col lg:bg-surface-100 dark:bg-surface-900 dark:lg:bg-surface-950">
-        <AppSidebar />
+        <AppSidebar
+            :open="unref(sidebar.open)"
+            :open-mobile="unref(sidebar.openMobile)"
+            :is-mobile="unref(sidebar.isMobile)"
+        />
 
-        <main class="flex flex-1 flex-col pb-2 lg:min-w-0 lg:pl-64 lg:pr-2 lg:pt-2">
+        <main
+            ref="main-content"
+            class="flex flex-1 flex-col pb-2 transition lg:min-w-0 lg:pr-2 lg:pt-2"
+        >
             <div
                 class="lg:shadow-xs grow p-6 lg:rounded-lg lg:bg-surface-0 lg:p-10 lg:ring-1 lg:ring-surface-950/5 dark:lg:bg-surface-900 dark:lg:ring-surface-0/10"
             >
-                <div>
-                    <Button label="Menu" />
+                <div class="text-right">
+                    <Button
+                        label="Menu"
+                        @click.prevent="sidebar.toggleSidebar"
+                    />
                 </div>
                 <div class="mx-auto max-w-6xl">
                     <slot />
